d10fdda52be0b9c4321f0c0f968efc3d62211b8d
Takuya Kuruma <tk.kuruma@gmail.com>
2014-08-20 09:09:14 +0900

    chatwork->slack

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ config = YAML.load_file(File.expand_path('../config.yml', __FILE__))
 
 AWS.config(config["aws"])
 cw = AWS::CloudWatch.new
-message = "今月のこれまでに使ったAWSの費用はこれくらいになったぞ\n-------------------\n"
+#message = "今月のこれまでに使ったAWSの費用はこれくらいになったぞ\n-------------------\n"
+message = "AWS COST REPORT\n---------------------\n"
 message += cw.metrics.filter("namespace","AWS/Billing").map do |m|
   st = m.statistics(start_time: Time.now-24.hours, end_time: Time.now, statistics: ["Average"], period: 60*60*24)
   service_name = m.dimensions.detect{|d| d[:name]=="ServiceName"}.try{|d| d[:value]} || "Total"
@@ -14,4 +15,6 @@ end.sort{|a,b| b[:currency]<=>a[:currency]}.map do |h|
   "#{h[:name]}: $#{h[:currency].round(2)}"
 end.join("\n")
 
-Chatwork.new(config["chatwork"]).message(message)
+#Chatwork.new(config["chatwork"]).message(message)
+notifier = Slack::Notifier.new "anipla", "0A8mr80kiCgK6uhdlK4UGySJ"
+notifier.ping message
